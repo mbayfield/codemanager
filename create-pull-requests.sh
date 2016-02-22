1c44244a8c10fc1707872cf0820d594f578e0808
mbayfield <mbayfield@gmail.com>
2016-02-22 16:50:56 +1100

    expand process
    includes:
    - add modified files
    - check if branch is ahead of master
    - update deps
    - bump version
    - push changes
    - create pull request
    - publish to npm

--- create-pull-requests.sh
+++ create-pull-requests.sh
@@ -5,8 +5,6 @@ path=$(pwd)
 # read -p "Update dependencies? [yn]: " updateDeps
 cd ..
 # echo "commit package changes"
-read -p "Push changes? [yn]: " push
-read -p "Create pull-requests? [yn]: " answer
 for f in */; do
 	if [[ -d $f ]]; then
 		if [[ -d $f/.git/ ]]; then
@@ -15,20 +13,59 @@ for f in */; do
 			
 			git remote -v | grep 'upstream' &> /dev/null && {
 				
-				# check if the pull-request should be made
-				if [[ $push = y ]]; 
+				git status -v
+				
+				read -p "Add modified files? [yn]: " modified
+				if [[ $modified = y ]]; 
 					then
-						# push the changes
-						git push origin master
-						echo "pushed changes"
+						git add -A
+						git commit
 				fi
 				
-				
-				if [[ $answer = y ]]; 
-					then
-						stash pull-request master upstream/master
-						echo "pull-request created"
-				fi
+				git status -v | grep 'Your branch is ahead' &> /dev/null && {
+					
+					ncu 
+					ncu | grep 'upgrade package.json' &> /dev/null && {
+						read -p "Update & commit dependencies? [yn]: " updateDeps
+						if [[ $updateDeps = 'y' ]];
+							then
+								ncu -u
+								git commit -m "package updates" -- package.json
+						fi
+					}
+					
+					read -p "Bump version? [yn]: " bump
+					if [[ $bump = y ]]; 
+						then
+							npm version patch
+							echo "version patched"
+					fi
+					
+					read -p "Push changes? [yn]: " push
+					# check if the pull-request should be made
+					if [[ $push = y ]]; 
+						then
+							# push the changes
+							git push origin master
+							echo "pushed changes"
+					fi
+					
+					read -p "Create pull-requests? [yn]: " pull
+					if [[ $pull = y ]]; 
+						then
+							stash pull-request master upstream/master
+							echo "pull-request created"
+					fi
+					
+					echo "Local revision: " && npm run revision
+					echo "NPM revision: " && npm info | jsawk -s 'return this.version'
+					read -p "Publish to npm? [yn]: " npmPublish	
+					if [[ $npmPublish = y ]]; 
+						then
+							npm publish
+							echo "published"
+					fi
+				}
 			}
 			cd ..
 		fi
